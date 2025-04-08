241f461a8be1e0fa92216b32c69c8d72b9a26e21
lfernando-cn <lfernandocn.dev@gmail.com>
2025-04-08 07:49:41 -0300

    fix: disable window resizing on main screen

--- src/main/java/com/github/lfernandocn/pharmamanager/Main.java
+++ src/main/java/com/github/lfernandocn/pharmamanager/Main.java
@@ -9,10 +9,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/pharmamanager/MenuPrincipal.fxml"));
-        Scene scene = new Scene(loader.load());
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/github/lfernandocn/pharmamanager/MenuPrincipal.fxml"));
+        Scene scene = new Scene(loader.load(),800,600);
         stage.setTitle("Sistema da Farmácia");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,12 @@ module com.github.lfernandocn.pharmamanager {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.github.lfernandocn.pharmamanager to javafx.fxml;
+    opens com.github.lfernandocn.pharmamanager.controllers to javafx.fxml; // necessário se os controllers estiverem aqui
+
     exports com.github.lfernandocn.pharmamanager;
+    exports com.github.lfernandocn.pharmamanager.controllers;
+    exports com.github.lfernandocn.pharmamanager.models;
+    exports com.github.lfernandocn.pharmamanager.utils;
+    exports com.github.lfernandocn.pharmamanager.repositorys;
 }
